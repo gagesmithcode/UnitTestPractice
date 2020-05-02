a7877185dc5d5883ec763a620377b533bf7817d9
Gage Smith <gage.smith.andrew@gmail.com>
2020-05-01 20:20:45 -0700

    checking a random list sorted descending.

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "Practice.h"
+#include <cstdlib>
 
 class PracticeTest : public ::testing::Test
 {
@@ -29,16 +30,35 @@ TEST(PracticeTest, is_not_palindrome)
     ASSERT_FALSE(actual);
 }
 
-TEST(PracticeTest, check_simple_sort)
+TEST(PracticeTest, check_ascending_list)
 {
 	Practice obj;
 	int first = 1, second = 2, third = 3;
 	obj.sortDescending(first, second, third);
 	bool flag;
-	if(first > second)
+	if(first > second && second > third)
+		flag = true;
+	else
+		flag = false;
+	//Should check for ASSERT_TRUE to confirm it works for ascending but it is a bug and
+	//ascending list is a end case sort descending doesn't handle
+	ASSERT_FALSE(flag);
+}
+
+TEST(PracticeTest, check_random_list)
+{
+	Practice obj;
+	int first = rand()%999;
+	int second = rand()%999;
+	int third = rand()%999;
+
+	obj.sortDescending(first, second, third);
+	bool flag;
+	if(first > second && second > third)
 		flag = true;
 	else
 		flag = false;
 
-	ASSERT_FALSE(flag);
+	ASSERT_TRUE(flag);
+
 }
